fd280c8d04c17b3d8588aee2d2b1994d24515fa0
Javad Asadi <force1267@gmail.com>
2019-07-31 15:19:30 +0430

    added project manager

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import app from "./app.js";
 import Editor from "./extensions/editor/index.js";
 import UI from "./extensions/ui/index.js";
+import ProjectManager from "./extensions/projectManager/index.js";
 
 // read config here
 const config = { editor: { initedVar: 12 }, ui: { name: "hexa" } };
@@ -9,6 +10,8 @@ const ui = UI(config.ui);
 app.use(ui);
 const editor = Editor(config.editor);
 app.use(editor);
+const projectManager = ProjectManager(config.projectManager);
+app.use(projectManager);
 
 app.start().then(function main() {
     // main function here
